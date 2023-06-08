0aba2f608f558cc3263258325c493beb3b415e08
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-06-08 23:19:54 +0200

    99

---
+++ lib/View/widget/onboarding/next_bottun.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../controllers/onboarding/onBoarding_controller.dart';
+
+class NextButtomWidget extends GetView<OnBoardingControllerImp> {
+  NextButtomWidget({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+  void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        InkWell(
+          onTap: () {
+            controller.nextPage();
+          },
+          child: const Text(
+            "Next",
+            style: TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 18,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
+      ],
+    );
+  }
+}
