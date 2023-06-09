a4f9019a5669083c31d8c75fd356cf2d49a11101
hossam <hossamcode2023@gmail.com>
2023-06-09 09:08:22 +0300

    create trainer controller

--- lib/View/Pages/review/trainerDetailPage.dart
+++ lib/View/Pages/review/trainerDetailPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:medicalty/View/Pages/review/reviewsPage.dart';
 import '../../../constatnt/color_app.dart';
+import '../../../controllers/Trainer/trainer_controller.dart';
 import '../../../controllers/home_page/homeController.dart';
 import '../../widget/homePage/bottomWidget.dart';
 import '../../widget/homePage/reviewsListWidget.dart';
@@ -11,138 +12,146 @@ import '../accounts/appointment.dart';
 
 class TrainerDetail extends StatelessWidget {
   const TrainerDetail({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-      body: Container(
-          color: ColorApp.whiteColor2,
-          child: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              Row(crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              Container(
-                margin: EdgeInsets.only(top: 20.0),
-                child: CircleAvatar(
-                  backgroundColor: ColorApp.blackColor2.withOpacity(0.2),
-                  maxRadius: 20.0,
-                  minRadius: 20.0,
-                  child: InkWell(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: Container(
-                        margin: EdgeInsets.only(left: 7.0),
-                        child: Icon(Icons.arrow_back_ios,
-                            color: ColorApp.whiteColor2)),
-                  ),
-                ),
-              ),
-           
-              Container(
-                  margin: EdgeInsets.all(20.0),
-                  height: 217,
-                  child: Image.asset(
-                    'assets/images/doctor.png',
-                    fit: BoxFit.cover,
-                  )),
-                     SizedBox(width: 90,),
-            ],
-          ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 25.0),
-                height: 55.0,
-                //================ Name the doctor ===================================
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: const [
-                        Text(
-                          "Jennifer James",
-                          style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: 'OpenSans',
-                              letterSpacing: 1.3),
+    Get.put(TrainerDetailController);
+    return GetBuilder<TrainerDetailController>(
+      init: TrainerDetailController(),
+      builder: (controller) {
+        return SafeArea(
+          child: Scaffold(
+            body: Container(
+              color: ColorApp.whiteColor2,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        child: CircleAvatar(
+                          backgroundColor:
+                              ColorApp.blackColor2.withOpacity(0.2),
+                          maxRadius: 20.0,
+                          minRadius: 20.0,
+                          child: InkWell(
+                            onTap: () {
+                              Get.back();
+                            },
+                            child: Container(
+                                margin: EdgeInsets.only(left: 7.0),
+                                child: Icon(Icons.arrow_back_ios,
+                                    color: ColorApp.whiteColor2)),
+                          ),
                         ),
-                        Text(
-                          'Functional Strength',
-                          style: TextStyle(
-                              fontSize: 13.0,
-                              fontWeight: FontWeight.w400,
-                              fontFamily: 'OpenSans',
-                              letterSpacing: 1.3),
+                      ),
+                      Container(
+                          margin: EdgeInsets.all(20.0),
+                          height: 217,
+                          child: Image.asset(
+                            'assets/images/doctor.png',
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                        width: 90,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 25.0),
+                    height: 55.0,
+                    //================ Name the doctor ===================================
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: const [
+                            Text(
+                              "Jennifer James",
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'OpenSans',
+                                  letterSpacing: 1.3),
+                            ),
+                            Text(
+                              'Functional Strength',
+                              style: TextStyle(
+                                  fontSize: 13.0,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: 'OpenSans',
+                                  letterSpacing: 1.3),
+                            ),
+                          ],
+                        ),
+                        Expanded(child: SizedBox()),
+                        // ============================ Call ==============================
+                        CircleAvatar(
+                          backgroundColor: ColorApp.blackBlueColor2,
+                          maxRadius: 28.0,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                                margin: EdgeInsets.only(left: 3.0),
+                                child: Icon(
+                                  Icons.chat_bubble_outline_outlined,
+                                  color: ColorApp.whiteColor2,
+                                  size: 30,
+                                )),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: ColorApp.radioButtom,
+                          maxRadius: 28.0,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                                margin: EdgeInsets.only(left: 3.0),
+                                child: Icon(
+                                  Icons.phone_sharp,
+                                  color: ColorApp.blackBlueColor2,
+                                  size: 30,
+                                )),
+                          ),
                         ),
                       ],
                     ),
-                    Expanded(child: SizedBox()),
-                    // ============================ Call ==============================
-                    CircleAvatar(
-                      backgroundColor: ColorApp.blackBlueColor2,
-                      maxRadius: 28.0,
-                      child: InkWell(
-                        onTap: () {},
-                        child: Container(
-                            margin: EdgeInsets.only(left: 3.0),
-                            child: Icon(
-                              Icons.chat_bubble_outline_outlined,
-                              color: ColorApp.whiteColor2,
-                              size: 30,
-                            )),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    CircleAvatar(
-                      backgroundColor: ColorApp.radioButtom,
-                      maxRadius: 28.0,
-                      child: InkWell(
-                        onTap: () {},
-                        child: Container(
-                            margin: EdgeInsets.only(left: 3.0),
-                            child: Icon(
-                              Icons.phone_sharp,
-                              color: ColorApp.blackBlueColor2,
-                              size: 30,
-                            )),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  // ======================== Box Experinec ===============================
+                  BoxBottom_1(),
+                  // ======================== Reviews Text ====================================
+                  ReviewsText(),
+                  // ================= Count images Reviews ===================================
+                  CountReviews(),
+                  // ================= Reviews ListWidget ====================================
+                  ReviewsListCommenets(
+                      axis: Axis.horizontal,
+                      height: 146.0,
+                      leftMar: 10.0,
+                      bottomMar: 0.0),
+                  // ================= Bottom Widget ======================================
+                  Bottom_1Widget(
+                    text: 'Book an Appointment',
+                    textFont: 'OpenSans',
+                    ontap: () {
+                      Get.to(Appointment());
+                    },
+                  ),
+                  SizedBox(
+                    height: 47.0,
+                  )
+                ],
               ),
-              // ======================== Box Experinec ===============================
-              BoxBottom_1(),
-              // ======================== Reviews Text ====================================
-              ReviewsText(),
-              // ================= Count images Reviews ===================================
-              CountReviews(),
-              // ================= Reviews ListWidget ====================================
-              ReviewsListCommenets(
-                  axis: Axis.horizontal,
-                  height: 146.0,
-                  leftMar: 10.0,
-                  bottomMar: 0.0),
-              // ================= Bottom Widget ======================================
-              Bottom_1Widget(
-                text: 'Book an Appointment',
-                textFont: 'OpenSans',
-                ontap: () {
-                  Get.to(Appointment());
-                },
-              ),
-              SizedBox(
-                height: 47.0,
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
