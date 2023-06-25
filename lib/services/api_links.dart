a67f24ec7e753ccbc62688afcdec2b612420a1f7
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-06-25 19:13:17 +0200

    up

--- lib/services/api_links.dart
+++
@@ -1,5 +0,0 @@
-class ApiLinks{
-  static String loginAdmin = "$baseUrl/api/center/admin/login";
-static const String baseUrl = 'http://medicalty.space';
- static const String endPointCreateCenterUrl = 'api/center/save';
-}
