04756e109b3be976ac5bfbf42163942b3daf0959
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-06-25 18:56:59 +0200

    up

---
+++ lib/controllers/informations/new_expenses_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NewExpensesController extends GetxController{
+
+  String dropdownvalue = 'Item 1';
+
+  // List of items in our dropdown menu
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+  final formKey = GlobalKey<FormState>();
+
+  DateTime? selectedDateTime;
+  TextEditingController idExpenesesController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
+  TextEditingController timeController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  TextEditingController valueController = TextEditingController();
+  TextEditingController payController = TextEditingController();
+  TextEditingController accountingCodeController = TextEditingController();
+  TextEditingController customerAddressController = TextEditingController();
+}
