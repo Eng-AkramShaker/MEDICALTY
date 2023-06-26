5537816cedcf2e6bdcccbfe030d04eb9b05748b9
rawy <rawyxd1995@gmail.com>
2023-06-26 12:49:26 +0300

    first commit

--- lib/View/widget/textfield_screen/onBoarding_Text_widget.dart
+++ lib/View/widget/textfield_screen/onBoarding_Text_widget.dart
@@ -1,30 +1,32 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
 
 class OnBoardingTextWidget extends StatelessWidget {
-String text;
-double fontSize;
-FontWeight fontWeight;
-Color color;
-TextDecoration? textDecoration;
+  String text;
+  double fontSize;
+  FontWeight fontWeight;
+  Color color;
+  TextDecoration? textDecoration;
 
-OnBoardingTextWidget(
-{required this.text,
-required this.fontSize,
-required this.fontWeight,
-required this.color,
-this.textDecoration});
+  OnBoardingTextWidget(
+      {required this.text,
+        required this.fontSize,
+        required this.fontWeight,
+        required this.color,
+        this.textDecoration});
 
 
-@override
-Widget build(BuildContext context) {
-return Text(
-text,
-style: TextStyle(
-decoration: textDecoration,
-fontSize: fontSize,
-fontWeight: fontWeight,
-color: color,
-fontFamily: 'Montserrat'),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          decoration: textDecoration,
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          color: color,
+          fontFamily: 'Montserrat'),
+    );
+  }
 }
